0dfef374b8957e6264cc79a01d05b6aecbf36499
Mostafa Mazrouh <mostafa.mazrouh@gmail.com>
2023-05-12 15:23:43 +0200

    Initial Commit

---
+++ PropertyMap/PropertyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyMapApp.swift
+//  PropertyMap
+//
+//  Created by Mostafa Mazrouh on 2023-05-12.
+//
+
+import SwiftUI
+
+@main
+struct PropertyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
